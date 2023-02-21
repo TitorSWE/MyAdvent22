786af9253740e201f1f3260f0b402ded1d9272b3
TitorSWE <victor.dumange@depinfonancy.net>
2023-02-21 16:04:12 +0000

    part one done

--- day17/day17.js
+++ day17/day17.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var ipt = fs.readFileSync("input-shape.txt", 'utf8').split('\n\n');
+let instructions = fs.readFileSync("input17.txt", 'utf8');
 
 let shapes = ipt.map(element => element.split('\n'));
 console.log(shapes);
@@ -20,15 +21,41 @@ let cave = Object.create(Cave);
 cave.high = 0;
 cave.matrix = [[1,1,1,1,1,1,1,1,1]];
 
-let firstShape = Object.create(Shape);
-firstShape.down = cave.high + 4;
-firstShape.up = firstShape.down + shapes[2].length;
-for (let i=cave.high; i<firstShape.up; i++){
-    cave.matrix.push([1,0,0,0,0,0,0,0,1]);
+
+let countRock = 0;
+let count = 0;
+while(countRock < 2022){
+    let shape = Object.create(Shape);
+    shape.down = cave.high + 4;
+    shape.up = shape.down + shapes[countRock % shapes.length].length -1;
+    for (let i=cave.high; i<shape.up; i++){
+        cave.matrix.push([1,0,0,0,0,0,0,0,1]);
+    }
+    shape.listPoints = findPoints(shapes[countRock % shapes.length], cave);
+    let stuck = false;
+    while (!stuck){
+        if (instructions.charAt(count%instructions.length) == '<'){
+            moveLeft(shape, cave);
+        }
+        if (instructions.charAt(count%instructions.length) == '>'){
+            moveRight(shape, cave);
+        }
+        stuck = fall(shape, cave);
+        count += 1;
+    }
+
+    if (shape.up > cave.high){
+        cave.high = shape.up;
+    }
+    
+    for (let i=0; i<shape.listPoints.length; i++){
+        let [x,y] = shape.listPoints[i];
+        cave.matrix[y][x] = 1;
+    }
+    countRock += 1;
 }
-firstShape.listPoints = findPoints(shapes[2], cave);
-translation(firstShape,[0,-3]);
-console.log(conflict(firstShape, cave));
+
+console.log(cave.high);
 
 function findLeft(stringShape){
     for (let i=0; i<stringShape.length; i++){
@@ -43,7 +70,7 @@ function findPoints(stringShape, cave){
     for (let i=0 ; i<stringShape.length; i++){
         for (let j=0; j<stringShape[0].length; j++){
             if (stringShape[i].charAt(j) == '#'){
-                let x = j + 2;
+                let x = j + 3;
                 let y = cave.high + 4 +(stringShape.length - 1 - i);
                 arrayIndex.push([x,y]);
             }
@@ -69,3 +96,29 @@ function conflict(shape, cave){
     return false;
 }
 
+function moveLeft(shape, cave){
+    translation(shape, [-1,0]);
+    if (conflict(shape, cave)){
+        translation(shape, [1,0]);
+    }
+}
+
+function moveRight(shape, cave){
+    translation(shape, [1,0]);
+    if (conflict(shape, cave)){
+        translation(shape, [-1,0]);
+    }
+}
+
+function fall(shape, cave){
+    translation(shape, [0,-1]);
+    shape.up -= 1;
+    shape.down -=1;
+    if (conflict(shape, cave)){
+        translation(shape, [0,1]);
+        shape.up += 1;
+        shape.down +=1;
+        return true;
+    }
+    return false;
+}
